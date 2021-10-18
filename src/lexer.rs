a65aefc50770d0ddb86b3df0251b981fc02d7c9c
mm1995tk <jimi.woodstock1995@gmail.com>
2021-10-19 04:04:42 +0900

    修正

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,42 +1,36 @@
+use std::str::Bytes;
+
 use crate::token::{Token, TokenType::*};
 
 #[derive(Debug)]
 pub struct Lexer<'a> {
-    pub input: &'a str,
-    pub position: u32,
-    pub read_position: u32,
+    pub input: Bytes<'a>,
     pub ch: u8,
 }
 
 impl Lexer<'_> {
     pub fn new(input: &str) -> Lexer {
         Lexer {
-            input,
-            position: 0,
-            read_position: 0,
+            input: input.bytes(),
             ch: 0,
         }
     }
     pub fn ch_byte_to_str(&self) -> String {
-        std::char::from_u32(self.ch as u32).unwrap().to_string()
+        std::str::from_utf8(&vec![self.ch]).unwrap().to_string()
     }
 
     pub fn read_char(&mut self) {
-        let lexer_inpu_len = self.input.len() as u32;
-        self.ch = if self.read_position >= lexer_inpu_len {
-            0
+        self.ch = if let Some(v) = self.input.next() {
+            v
         } else {
-            self.input.as_bytes()[self.read_position as usize]
-        };
-        self.position = self.read_position;
-        self.read_position += 1;
+            0
+        }
     }
 
     pub fn next_token(&mut self) -> Token {
         self.read_char();
         let literal = self.ch_byte_to_str();
-        let ref_literal = &literal as &str;
-        let token_type = match ref_literal {
+        let token_type = match &literal as &str {
             "=" => ASSIGN,
             ";" => SEMICOLON,
             "(" => LPAREN,
@@ -45,16 +39,10 @@ impl Lexer<'_> {
             "+" => PLUS,
             "{" => LBRACE,
             "}" => RBRACE,
-            _ => {
-                if self.ch == 0 {
-                    EOF
-                } else {
-                    match ref_literal {
-                        "p" => EOF,
-                        _ => ILLEGAL
-                    }
-                }
-            }
+            _ => match self.ch {
+                0 => EOF,
+                _ => ILLEGAL,
+            },
         };
         Token {
             token_type,
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use interpreter_in_rust::{lexer::Lexer};
+use interpreter_in_rust::lexer::Lexer;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let statement = args.get(1);
 
     let mut lexer = Lexer::new(statement.unwrap());
-    for _ in 0..statement.unwrap().len()+1 {
+    for _ in 0..statement.unwrap().len() + 1 {
         let token = lexer.next_token();
         println!("{:?}", token)
     }
