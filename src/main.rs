fb9ace2a3da10aecb8c58001f23bae0cb582f7f6
mm1995tk <jimi.woodstock1995@gmail.com>
2021-10-30 23:12:21 +0900

    ファイルから入力できるように変更

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use interpreter_in_rust::lexer::Lexer;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let statement = args.get(1);
-
-    let mut lexer = Lexer::new(statement.unwrap());
-    for _ in 0..statement.unwrap().len() + 1 {
-        let token = lexer.next_token();
-        println!("{:?}", token)
+    for arg in &args[1..] {
+        let mut lexer = Lexer::new(arg);
+        for _ in 0..arg.len() + 1 {
+            let token = lexer.next_token();
+            println!("{:?}", token)
+        }
     }
+
+
 }
