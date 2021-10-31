b9091ba59724636993d2bdd7bc5f19b72bd17c5f
mm1995tk <jimi.woodstock1995@gmail.com>
2021-11-01 01:07:41 +0900

    main関数の修正

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use interpreter_in_rust::lexer::Lexer;
+use interpreter_in_rust::token::TokenType::EOF;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -6,7 +7,17 @@ fn main() {
         let mut lexer = Lexer::new(arg);
         for _ in 0..arg.len() + 1 {
             let token = lexer.next_token();
-            println!("{:?}", token)
+            match token {
+                Ok(ok_token) if ok_token.token_type == EOF => {
+                    if ok_token.token_type == EOF {
+                        break;
+                    }
+                }
+                Ok(ok_token) => {
+                    println!("{:?}", ok_token)
+                }
+                Err(_) => panic!(),
+            }
         }
     }
 }
